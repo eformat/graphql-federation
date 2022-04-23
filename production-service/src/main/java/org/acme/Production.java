dfd842f24fe778a36c5d3f3bda04c9f933ee6f11
Mike Hepburn <eformat@gmail.com>
2022-04-23 11:02:20 +1000

    🦃 not exending production 🦃

--- production-service/src/main/java/org/acme/Production.java
+++ production-service/src/main/java/org/acme/Production.java
@@ -1,13 +1,9 @@
 package org.acme;
-
-import io.smallrye.graphql.api.federation.External;
-import io.smallrye.graphql.api.federation.Key;
 import org.eclipse.microprofile.graphql.Id;
 import org.eclipse.microprofile.graphql.NonNull;
 
 import java.util.List;
 
-@Key(fields = "productionId")
 public class Production {
 
     public @NonNull @Id String productionId;
